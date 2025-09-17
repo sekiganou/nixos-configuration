e9d7d55b85ed73f13f61092b1e4153a036940f75
Alessio Bagno <alessiobagno@gmail.com>
2025-09-17 13:23:06 +0200

    fix some errors

--- hosts/laptop-asus/configuration.nix
+++ hosts/laptop-asus/configuration.nix
@@ -9,7 +9,7 @@
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
       inputs.home-manager.nixosModules.default
-      ../modules/home-manager/vscode.nix
+      ../../modules/home-manager/vscode.nix
     ];
 
   # Bootloader.
--- modules/home-manager/vscode.nix
+++ modules/home-manager/vscode.nix
@@ -1,3 +1,4 @@
+{ config, lib, pkgs, ...}:
 {
   programs.vscode = {
     enable = true;
